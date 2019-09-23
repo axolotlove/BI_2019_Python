8b9d57ab3f2e73c11ed5f65b7dae576b8ef4ec8b
Dmitry Alexeev <st037587@student.spbu.ru>
2019-09-23 21:49:48 +0300

    travys added

--- Homeworks/calculator.py
+++ Homeworks/calculator.py
@@ -1,18 +1,21 @@
-number1 = int(input())
+print("Введите первое число, оператор (+ - / *) и второе число")
+number1 = float(input())
 operator = input()
-number2 = int(input())
+number2 = float(input())
 result = 0
 if operator == '+':
     result = number1 + number2
-    print(result);
+    print(result)
 elif operator == '-':
     result = number1 - number2
-    print(result);
-elif operator == '/':
+    print(result)
+elif operator == '/' & number2 != 0:
     result = number1 / number2
-    print(result);
+    print(result)
+elif operator == '/' & number2 == 0:
+    print("Невозможно деление на 0")
 elif operator == '*':
     result = number1 * number2
-    print(result);
+    print(result)
 else:
     print('Операция не поддерживается')
