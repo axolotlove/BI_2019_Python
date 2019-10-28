2919a770dc2261063640b94df332e3238876d811
Dmitry Alexeev <st037587@student.spbu.ru>
2019-10-29 02:22:46 +0300

    2 checkio fixed

--- Homeworks/4th_homework/2_checkio.py
+++ Homeworks/4th_homework/2_checkio.py
@@ -2,7 +2,7 @@ def group_equal(els):
     seen = {}
     n = 0
     for i in range(len(els) - 1):
-        if els[i] == els[i+1]:
+        if els[i] == els[i + 1]:
             if els[i] not in seen:
                 seen[els[i]] = [els[i]]
             else:
@@ -12,7 +12,7 @@ def group_equal(els):
                 seen[els[i]].append(els[i])
             else:
                 n += 1
-                seen['solo'+str(n)] = [els[i]]
+                seen['solo' + str(n)] = [els[i]]
     if els[len(els) - 1] == els[len(els) - 2]:
         seen[els[len(els) - 1]].append(els[len(els) - 1])
     else:
@@ -22,6 +22,7 @@ def group_equal(els):
     for value in seen.values():
         result.append(value)
     return result
+
 
 els = [str(i) for i in input().split(',')]
 print(group_equal(els))
